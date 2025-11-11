f5f81347313b9b57cff5160ca91441e4aeeec912
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-11-11 17:41:43 +0400

    Updated images

---
+++ src/assets/images/index.js
@@ -0,0 +1,13 @@
+// Logo
+export { default as logo } from './impera-temp.png';
+
+// Background
+export { default as background } from './main.jpg';
+
+// Web-Art
+export { default as thanku } from './thanku.png';
+export { default as document } from './art/document.png';
+export { default as checklist } from './art/checklist.png';
+
+// Photos
+export { default as photo1 } from './photos/olga.jpeg';
