abeb6852c44f005cb5dc2dfee575619876c6c8dc
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-11-26 17:52:52 +0400

    Updated images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -4,6 +4,14 @@ export { default as logo } from './logo.webp';
 // Background
 export { default as background } from './main.jpg';
 
+// Backgrounds
+export { default as background1 } from './backgrounds/impera-background-1.jpg';
+export { default as background2 } from './backgrounds/impera-background-2.jpg';
+export { default as background3 } from './backgrounds/impera-background-3.jpg';
+export { default as background4 } from './backgrounds/impera-background-4.jpg';
+export { default as background5 } from './backgrounds/impera-background-5.jpg';
+export { default as background6 } from './backgrounds/impera-background-6.jpg';
+
 // Web-Art
 export { default as thanku } from './thanku.png';
 export { default as document } from './art/document.png';
@@ -11,6 +19,8 @@ export { default as checklist } from './art/checklist.png';
 
 // Photos
 export { default as photo1 } from './photos/olga.jpeg';
+export { default as photo2 } from './photos/olga-2.webp';
+export { default as photo3 } from './photos/anastasia.png';
 
 // Projects
 // Boss-Lounge
