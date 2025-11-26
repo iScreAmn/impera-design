3ad96eb88ad085ec8218d86df92cbdd8ad34fff1
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-11-26 17:53:57 +0400

    Updated Home section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,14 @@ import Header from '../Header/Header';
 import Navigation from '../Navigation/Navigation';
 import Hero from '../Hero/Hero';
 import Calculator from '../Calculator/Calculator';
+import ProjectsCollage from '../ProjectsCollage/ProjectsCollage';
+import ServicesSection from '../ServicesSection/ServicesSection';
+import Advantage from '../Advantage/Advantage';
+import Creator from '../Creator/Creator';
+import CTA from '../CTA/CTA';
 import { calculatorData } from '../../data/calculatorData';
+import { aboutData } from '../../data/aboutData';
+import { ctaData } from '../../data/ctaData';
 import './Home.css';
 
 function Home() {
@@ -12,6 +19,17 @@ function Home() {
       <Navigation />
       <Hero />
       <Calculator {...calculatorData} />
+      <section className="home__projects">
+        <div className="home__projects-header">
+          <h2 className="home__projects-title">Реализованные проекты Impera Studio</h2>
+          <p className="home__projects-subtitle">Наши работы говорят сами за себя</p>
+        </div>
+        <ProjectsCollage />
+      </section>
+      <ServicesSection />
+      <Advantage />
+      <Creator {...aboutData} />
+      <CTA {...ctaData} />
     </div>
   );
 }
