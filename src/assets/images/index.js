3d834ac622a16ee2cc6e635924f73efb72227437
Dimitri. J <jmukhadze.dimitri@gmail.com>
2026-01-16 11:01:44 +0400

    Uploaded project images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -27,47 +27,185 @@ export { default as photo5 } from './photos/olga-4.jpg';
 
 // Projects
 // Boss-Lounge
-export { default as bossLounge1 } from './projects/boss-lounge/boss1.jpg';
-export { default as bossLounge2 } from './projects/boss-lounge/boss2.webp';
-export { default as bossLounge3 } from './projects/boss-lounge/boss3.webp';
-export { default as bossLounge4 } from './projects/boss-lounge/boss4.jpg';
+export { default as bossLounge1 } from './projects/boss-lounge/boss-1.jpg';
+export { default as bossLounge2 } from './projects/boss-lounge/boss-2.jpg';
+export { default as bossLounge3 } from './projects/boss-lounge/boss-3.jpg';
+export { default as bossLounge4 } from './projects/boss-lounge/boss-4.jpg';
+export { default as bossLounge5 } from './projects/boss-lounge/boss-5.jpg';
+export { default as bossLounge6 } from './projects/boss-lounge/boss-6.jpg';
+export { default as bossLounge7 } from './projects/boss-lounge/boss-7.jpg';
+export { default as bossLounge8 } from './projects/boss-lounge/boss-8.jpg';
+export { default as bossLounge9 } from './projects/boss-lounge/boss-9.jpg';
+export { default as bossLounge10 } from './projects/boss-lounge/boss-10.jpg';
+export { default as bossLounge11 } from './projects/boss-lounge/boss-11.jpg';
+export { default as bossLounge12 } from './projects/boss-lounge/boss-12.jpg';
+export { default as bossLounge13 } from './projects/boss-lounge/boss-13.jpg';
+export { default as bossLounge14 } from './projects/boss-lounge/boss-14.jpg';
+export { default as bossLounge15 } from './projects/boss-lounge/boss-15.jpg';
+export { default as bossLounge16 } from './projects/boss-lounge/boss-16.jpg';
 
 // Lesnoi
-export { default as lesnoiPark1 } from './projects/lesnoi/lesnoi1.webp';
-export { default as lesnoiPark2 } from './projects/lesnoi/lesnoi2.webp';
-export { default as lesnoiPark3 } from './projects/lesnoi/lesnoi3.webp';
-export { default as lesnoiPark4 } from './projects/lesnoi/lesnoi4.webp';
+export { default as lesnoiPark1 } from './projects/lesnoi/lesnoi-1.jpg';
+export { default as lesnoiPark2 } from './projects/lesnoi/lesnoi-2.jpg';
+export { default as lesnoiPark3 } from './projects/lesnoi/lesnoi-3.jpg';
+export { default as lesnoiPark4 } from './projects/lesnoi/lesnoi-4.jpg';
+export { default as lesnoiPark5 } from './projects/lesnoi/lesnoi-5.jpg';
+export { default as lesnoiPark6 } from './projects/lesnoi/lesnoi-6.jpg';
+export { default as lesnoiPark7 } from './projects/lesnoi/lesnoi-7.jpg';
+export { default as lesnoiPark8 } from './projects/lesnoi/lesnoi-8.jpg';
+export { default as lesnoiPark9 } from './projects/lesnoi/lesnoi-9.jpg';
+export { default as lesnoiPark10 } from './projects/lesnoi/lesnoi-10.jpg';
+export { default as lesnoiPark11 } from './projects/lesnoi/lesnoi-11.jpg';
+export { default as lesnoiPark12 } from './projects/lesnoi/lesnoi-12.jpg';
+export { default as lesnoiPark13 } from './projects/lesnoi/lesnoi-13.jpg';
+export { default as lesnoiPark14 } from './projects/lesnoi/lesnoi-14.jpg';
+export { default as lesnoiPark15 } from './projects/lesnoi/lesnoi-15.jpg';
+export { default as lesnoiPark16 } from './projects/lesnoi/lesnoi-16.jpg';
+export { default as lesnoiPark17 } from './projects/lesnoi/lesnoi-17.jpg';
+export { default as lesnoiPark18 } from './projects/lesnoi/lesnoi-18.jpg';
+export { default as lesnoiPark19 } from './projects/lesnoi/lesnoi-19.jpg';
 
-// Cafe Trophi
-export { default as cafeTrophi1 } from './projects/cafe-trophi/trophi1.jpg';
-export { default as cafeTrophi2 } from './projects/cafe-trophi/trophi2.jpg';
-export { default as cafeTrophi3 } from './projects/cafe-trophi/trophi3.jpg';
-export { default as cafeTrophi4 } from './projects/cafe-trophi/trophi4.jpg';
+// Thai Massage
+export { default as taiMassage1 } from './projects/thai/thai-1.jpg';
+export { default as taiMassage2 } from './projects/thai/thai-2.jpg';
+export { default as taiMassage3 } from './projects/thai/thai-3.jpg';
+export { default as taiMassage4 } from './projects/thai/thai-4.jpg';
+export { default as taiMassage5 } from './projects/thai/thai-5.jpg';
+export { default as taiMassage6 } from './projects/thai/thai-6.jpg';
+export { default as taiMassage7 } from './projects/thai/thai-7.jpg';
+export { default as taiMassage8 } from './projects/thai/thai-8.jpg';
+export { default as taiMassage9 } from './projects/thai/thai-9.jpg';
+export { default as taiMassage10 } from './projects/thai/thai-10.jpg';
+export { default as taiMassage11 } from './projects/thai/thai-11.jpg';
+export { default as taiMassage12 } from './projects/thai/thai-12.jpg';
+export { default as taiMassage13 } from './projects/thai/thai-13.jpg';
+export { default as taiMassage14 } from './projects/thai/thai-14.jpg';
+export { default as taiMassage15 } from './projects/thai/thai-15.jpg';
+export { default as taiMassage16 } from './projects/thai/thai-16.jpg';
+export { default as taiMassage17 } from './projects/thai/thai-17.jpg';
+
 
 // Kids Room
-export { default as kidsRoom1 } from './projects/kids-room/kids1.jpeg';
-export { default as kidsRoom2 } from './projects/kids-room/kids2.jpeg';
-export { default as kidsRoom3 } from './projects/kids-room/kids3.jpeg';
-export { default as kidsRoom4 } from './projects/kids-room/kids4.jpeg';
+export { default as kidsRoom1 } from './projects/kids-room/kids-1.jpeg';
+export { default as kidsRoom2 } from './projects/kids-room/kids-2.jpeg';
+export { default as kidsRoom3 } from './projects/kids-room/kids-3.jpeg';
+export { default as kidsRoom4 } from './projects/kids-room/kids-4.jpeg';
+export { default as kidsRoom5 } from './projects/kids-room/kids-5.jpeg';
+export { default as kidsRoom6 } from './projects/kids-room/kids-6.jpeg';
+export { default as kidsRoom7 } from './projects/kids-room/kids-7.jpeg';
+export { default as kidsRoom8 } from './projects/kids-room/kids-8.jpeg';
 
 // Slow Glow
-export { default as slowGlow1 } from './projects/slow-glow/slow1.jpg';
-export { default as slowGlow2 } from './projects/slow-glow/slow2.jpg';
-export { default as slowGlow3 } from './projects/slow-glow/slow3.jpg';
-export { default as slowGlow4 } from './projects/slow-glow/slow4.jpg';
+export { default as slowGlow1 } from './projects/slow-glow/slow-1.jpg';
+export { default as slowGlow2 } from './projects/slow-glow/slow-2.jpg';
+export { default as slowGlow3 } from './projects/slow-glow/slow-3.jpg';
+export { default as slowGlow4 } from './projects/slow-glow/slow-4.jpg';
+export { default as slowGlow5 } from './projects/slow-glow/slow-5.jpg';
+export { default as slowGlow6 } from './projects/slow-glow/slow-6.jpg';
+export { default as slowGlow7 } from './projects/slow-glow/slow-7.jpg';
+export { default as slowGlow8 } from './projects/slow-glow/slow-8.jpg';
 
 // Bulvar
-export { default as bulvar1 } from './projects/bulvar/bulvar1.jpg';
-export { default as bulvar2 } from './projects/bulvar/bulvar2.jpg';
-export { default as bulvar3 } from './projects/bulvar/bulvar3.jpg';
-export { default as bulvar4 } from './projects/bulvar/bulvar4.jpg';
+export { default as bulvar1 } from './projects/bulvar/bulvar-1.jpg';
+export { default as bulvar2 } from './projects/bulvar/bulvar-2.jpg';
+export { default as bulvar3 } from './projects/bulvar/bulvar-3.jpg';
+export { default as bulvar4 } from './projects/bulvar/bulvar-4.jpg';
+export { default as bulvar5 } from './projects/bulvar/bulvar-5.jpg';
+export { default as bulvar6 } from './projects/bulvar/bulvar-6.jpg';
+export { default as bulvar7 } from './projects/bulvar/bulvar-7.jpg';
+export { default as bulvar8 } from './projects/bulvar/bulvar-8.jpg';
+export { default as bulvar9 } from './projects/bulvar/bulvar-9.jpg';
+export { default as bulvar10 } from './projects/bulvar/bulvar-10.jpg';
+export { default as bulvar11 } from './projects/bulvar/bulvar-11.jpg';
+export { default as bulvar12 } from './projects/bulvar/bulvar-12.jpg';
+export { default as bulvar13 } from './projects/bulvar/bulvar-13.jpg';
 
-// Office Express
-export { default as office1 } from './projects/office/officeExpress1.jpg';
-export { default as office2 } from './projects/office/officeExpress2.jpg';
-export { default as office3 } from './projects/office/officeExpress3.jpg';
-export { default as office4 } from './projects/office/officeExpress4.jpg';
+// Office
+export { default as office1 } from './projects/office/office-1.jpg';
+export { default as office2 } from './projects/office/office-2.jpg';
+export { default as office3 } from './projects/office/office-3.jpg';
+export { default as office4 } from './projects/office/office-4.jpg';
+export { default as office5 } from './projects/office/office-5.jpg';
+export { default as office6 } from './projects/office/office-6.jpg';
+export { default as office7 } from './projects/office/office-7.jpg';
+export { default as office8 } from './projects/office/office-8.jpg';
+export { default as office9 } from './projects/office/office-9.jpg';
+export { default as office10 } from './projects/office/office-10.jpg';
+export { default as office11 } from './projects/office/office-11.jpg';
+export { default as office12 } from './projects/office/office-12.jpg';
+export { default as office13 } from './projects/office/office-13.jpg';
+export { default as office14 } from './projects/office/office-14.jpg';
+export { default as office15 } from './projects/office/office-15.jpg';
+export { default as office16 } from './projects/office/office-16.jpg';
+export { default as office17 } from './projects/office/office-17.jpg';
 
+// Dvorik
+export { default as dvorik1 } from './projects/dvorik/dvorik-1.jpg';
+export { default as dvorik2 } from './projects/dvorik/dvorik-2.jpg';
+export { default as dvorik3 } from './projects/dvorik/dvorik-3.jpg';
+export { default as dvorik4 } from './projects/dvorik/dvorik-4.jpg';
+export { default as dvorik5 } from './projects/dvorik/dvorik-5.jpg';
+export { default as dvorik6 } from './projects/dvorik/dvorik-6.jpg';
+export { default as dvorik7 } from './projects/dvorik/dvorik-7.jpg';
+export { default as dvorik8 } from './projects/dvorik/dvorik-8.jpg';
+export { default as dvorik9 } from './projects/dvorik/dvorik-9.jpg';
+
+// Hotel
+export { default as hotel1 } from './projects/hotel/1.jpg';
+export { default as hotel2 } from './projects/hotel/2.jpg';
+export { default as hotel3 } from './projects/hotel/3.jpg';
+export { default as hotel4 } from './projects/hotel/4.jpg';
+export { default as hotel5 } from './projects/hotel/5.jpg';
+export { default as hotel6 } from './projects/hotel/6.jpg';
+export { default as hotel7 } from './projects/hotel/7.jpg';
+export { default as hotel8 } from './projects/hotel/8.jpg';
+export { default as hotel9 } from './projects/hotel/9.jpg';
+export { default as hotel10 } from './projects/hotel/10.jpg';
+export { default as hotel11 } from './projects/hotel/11.jpg';
+export { default as hotel12 } from './projects/hotel/12.jpg';
+export { default as hotel13 } from './projects/hotel/13.jpg';
+export { default as hotel14 } from './projects/hotel/14.jpg';
+export { default as hotel15 } from './projects/hotel/15.jpg';
+export { default as hotel16 } from './projects/hotel/16.jpg';
+export { default as hotel17 } from './projects/hotel/17.jpg';
+export { default as hotel18 } from './projects/hotel/18.jpg';
+export { default as hotel19 } from './projects/hotel/19.jpg';
+export { default as hotel20 } from './projects/hotel/20.jpg';
+export { default as hotel21 } from './projects/hotel/21.jpg';
+export { default as hotel22 } from './projects/hotel/22.jpg';
+export { default as hotel23 } from './projects/hotel/23.jpg';
+export { default as hotel24 } from './projects/hotel/24.jpg';
+export { default as hotel25 } from './projects/hotel/25.jpg';
+export { default as hotel26 } from './projects/hotel/26.jpg';
+
+// Paratun
+export { default as paratun1 } from './projects/paratun/1.jpg';
+export { default as paratun2 } from './projects/paratun/2.jpg';
+export { default as paratun3 } from './projects/paratun/3.jpg';
+export { default as paratun4 } from './projects/paratun/4.jpg';
+export { default as paratun5 } from './projects/paratun/5.jpg';
+export { default as paratun6 } from './projects/paratun/6.jpg';
+export { default as paratun7 } from './projects/paratun/7.jpg';
+export { default as paratun8 } from './projects/paratun/8.jpg';
+export { default as paratun9 } from './projects/paratun/9.jpg';
+export { default as paratun10 } from './projects/paratun/10.jpg';
+export { default as paratun11 } from './projects/paratun/11.jpg';
+export { default as paratun12 } from './projects/paratun/12.jpg';
+export { default as paratun13 } from './projects/paratun/13.jpg';
+
+// Crazy-Brothers
+export { default as crazyBrothers1 } from './projects/crazy-brothers/1.jpg';
+export { default as crazyBrothers2 } from './projects/crazy-brothers/2.jpg';
+export { default as crazyBrothers3 } from './projects/crazy-brothers/3.jpg';
+export { default as crazyBrothers4 } from './projects/crazy-brothers/4.jpg';
+export { default as crazyBrothers5 } from './projects/crazy-brothers/5.jpg';
+export { default as crazyBrothers6 } from './projects/crazy-brothers/6.jpg';
+export { default as crazyBrothers7 } from './projects/crazy-brothers/7.jpg';
+export { default as crazyBrothers8 } from './projects/crazy-brothers/8.jpg';
+export { default as crazyBrothers9 } from './projects/crazy-brothers/9.jpg';
+export { default as crazyBrothers10 } from './projects/crazy-brothers/10.jpg';
+export { default as crazyBrothers11 } from './projects/crazy-brothers/11.jpg';
+export { default as crazyBrothers12 } from './projects/crazy-brothers/12.jpg';
 
 // Partners
 export { default as partner1 } from './partners/drev-uiut-black22.png';
