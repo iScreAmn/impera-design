5da1e35c38cfb8c38ea11497b084ff4d916336d8
Dimitri. J <jmukhadze.dimitri@gmail.com>
2026-02-10 22:20:38 +0400

    Updated deploy methods

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-})
+export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd(), '');
+  const apiTarget = env.VITE_API_TARGET || 'http://localhost:3001';
+
+  return {
+    plugins: [react()],
+    base: '/',
+    server: {
+      proxy: {
+        '/api': {
+          target: apiTarget,
+          changeOrigin: true,
+          secure: false
+        }
+      }
+    }
+  };
+});
